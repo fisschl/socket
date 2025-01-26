20021f0625c1796315b35468bd095bbfc88225bb
WangZhenghai <fisschl@163.com>
2025-01-26 11:21:26 +0800

    优化变量命名提示词

--- router/variables.ts
+++ router/variables.ts
@@ -3,9 +3,9 @@ import type OpenAI from "openai";
 import { MoonshotBaseClient } from "./translate.ts";
 
 const NamingPrompt = `
-你是一名程序员，你的任务是为一个字段命名。该字段可能指一个变量、函数、类、文件名等。
-接下来，我会给你一些描述或提示，请你根据描述的含义给出对应的命名，使用 PascalCase 格式。
-你的命名应该尽量简短而有意义。尽量给出多个备选的命名。多个命名之间用逗号分隔。
+你是一名程序员，你的任务是为一个字段、变量、类、文件或者函数命名。
+接下来，我会给你一些描述或提示词，请你根据描述的含义给出对应的命名。
+注意：你的命名应该尽量简短而有意义。尽量给出多个备选的命名。多个命名之间用逗号分隔。使用 PascalCase 格式输出
 `;
 
 export interface NamingRequest {
