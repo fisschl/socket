905c99ae5f2bd50ac965ec5e29d0aa0009dacb86
WangZhenghai <fisschl@163.com>
2025-02-25 14:16:34 +0800

    Add Socket.IO server with CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { api } from "./api";
+import { io } from "./socket";
+
+io.listen(4000);
+console.log("Socket.IO server is running on port 4000");
+
 const app = new Hono();
 
 app
